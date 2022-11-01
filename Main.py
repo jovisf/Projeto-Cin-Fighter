e7a2a131f199af6f88b7b45e3e0b5a7d1173296e
sergiolbarreto <sergioleonardo68@gmail.com>
2022-10-31 23:01:58 -0300

    Implementa menu

--- Main.py
+++ Main.py
@@ -1,14 +1,16 @@
 import pygame
+import sys
+from button import Button
 from fighter import Fighter
 
 pygame.init()
 
 #create game window
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 600
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Brawler")
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+SCREEN = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Cin Fighter")
+BG = pygame.image.load("assets/Background.png")
 
 #set framerate
 clock = pygame.time.Clock()
@@ -27,72 +29,167 @@ score = [0, 0]#player scores. [P1, P2]
 round_over = False
 ROUND_OVER_COOLDOWN = 2000
 
-
-
-
+def get_font(size): # Returns Press-Start-2P in the desired size
+    return pygame.font.Font("assets/font.ttf", size)
 
 #load background image
 bg_image = pygame.image.load("back.jpg").convert_alpha()
 
 
-
-
-
 #function for drawing text
 def draw_text(text, font, text_col, x, y):
   img = font.render(text, True, text_col)
-  screen.blit(img, (x, y))
+  SCREEN.blit(img, (x, y))
 
 #function for drawing background
 def draw_bg():
   scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
-  screen.blit(scaled_bg, (0, 0))
+  SCREEN.blit(scaled_bg, (0, 0))
 
 #function for drawing fighter health bars
 def draw_health_bar(health, x, y):
   ratio = health / 100
-  pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 404, 34))
-  pygame.draw.rect(screen, RED, (x, y, 400, 30))
-  pygame.draw.rect(screen, GREEN, (x, y, 400 * ratio, 30))
+  pygame.draw.rect(SCREEN, WHITE, (x - 2, y - 2, 404, 34))
+  pygame.draw.rect(SCREEN, RED, (x, y, 400, 30))
+  pygame.draw.rect(SCREEN, GREEN, (x, y, 400 * ratio, 30))
 
 
 #create two instances of fighters
 fighter_1 = Fighter(1, 200, 310, False)
 fighter_2 = Fighter(2, 700, 310, True)
 
+def play_teste():
+    while True:
+        PLAY_MOUSE_POS = pygame.mouse.get_pos()
 
+        SCREEN.fill("black")
+
+        PLAY_TEXT = get_font(45).render("This is the PLAY screen.", True, "White")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
+
+        PLAY_BACK = Button(image='assets/exit_btn.png', pos=(640, 460), 
+                            text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
+
+        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
+        PLAY_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
+                    main_menu()
+
+        pygame.display.update()
+
+
+def options():
+    while True:
+        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("white")
+
+        OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True, "Black")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
+
+        OPTIONS_BACK = Button(image=None, pos=(640, 460), 
+                            text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+        OPTIONS_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                    main_menu()
+
+        pygame.display.update()
+
+def main_menu():
+    while True:
+        SCREEN.blit(BG, (0, 0))
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250), 
+                            text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(640, 400), 
+                            text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550), 
+                            text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+
+        SCREEN.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(SCREEN)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    play()
+                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    options()
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
 
 #game loop
-run = True
-while run:
+def play():
+  while True:
 
-  clock.tick(FPS)
+    clock.tick(FPS)
 
-  #draw background
-  draw_bg()
-
-  #show player stats
-  draw_health_bar(fighter_1.health, 20, 20)
-  draw_health_bar(fighter_2.health, 580, 20)
-
-
-  #draw fighters
-  fighter_1.draw(screen, RED)
-  fighter_2.draw(screen, BLUE)
-
-  fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2)
-  fighter_2.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_1)
+    #draw background
+    draw_bg()
+    #show player stats
+    draw_health_bar(fighter_1.health, 20, 20)
+    draw_health_bar(fighter_2.health, 580, 20)
 
 
+    #draw fighters
+    fighter_1.draw(SCREEN, RED)
+    fighter_2.draw(SCREEN, BLUE)
 
-  #event handler
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      run = False
+    fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN, fighter_2)
+    fighter_2.move(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN, fighter_1)
+    
+    # back button  
+    # exit_button.draw(SCREEN)
+    PLAY_MOUSE_POS = pygame.mouse.get_pos()
+    PLAY_BACK = Button(image='assets/exit_btn.png', pos=(1150, 80), 
+                            text_input="", font=get_font(24), base_color="Black", hovering_color="Green")
+
+    PLAY_BACK.changeColor(PLAY_MOUSE_POS)
+    PLAY_BACK.update(SCREEN)
+
+    #event handler
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        break
+      if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
+                    main_menu()
 
 
-  #update display
-  pygame.display.update()
+    #update display
+    pygame.display.update()
 
-#exit pygame
-pygame.quit()
+  #exit pygame
+  pygame.quit()
+
+main_menu()
